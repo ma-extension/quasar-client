d779a8bd84612afb77bd12b313a2417532368721
ronald <ronald-farias@outlook.com>
2019-02-27 22:19:38 -0300

    add reader to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import reader from './reader'
 // import example from './module-example'
 
 Vue.use(Vuex)
@@ -14,6 +14,7 @@ export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
       // example
+      reader
     }
   })
 
