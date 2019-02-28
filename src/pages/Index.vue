cde7a8f36a6eeaed4afeea17a644e0bd49c3ee28
ronald <ronald-farias@outlook.com>
2019-02-27 22:19:57 -0300

    removes list from index and call ListReader component

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,42 +1,6 @@
 <template>
   <q-page id="index" class="flex flex-center">
-    <q-list bordered class="rounded-borders">
-      <q-expansion-item>
-      <template v-slot:header>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://onepiece-ex.com.br/favicon/favicon-32x32.png" alt="">
-          </q-avatar>
-        </q-item-section>
-        <q-item-section>onepiece-ex</q-item-section>
-      </template>
-        <q-card>
-          <q-list>
-            <q-expansion-item
-              label="One Piece"
-            >
-              <q-list>
-                <q-item>cap1</q-item>
-              </q-list>
-            </q-expansion-item>
-          </q-list>
-        </q-card>
-      </q-expansion-item>
-
-      <q-expansion-item
-        expand-separator
-        icon="signal_wifi_off"
-        label="Wifi settings"
-      >
-        <q-card>
-          <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
-    </q-list>
+    <ListReader />
   </q-page>
 </template>
 
@@ -47,7 +11,11 @@
 </style>
 
 <script>
+import ListReader from '../components/ListReader.vue'
 export default {
+  components: {
+    ListReader
+  },
   name: 'PageIndex'
 }
 </script>
