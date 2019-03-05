6bfe2f40ae31ccc531b7556704fa3944fa1cf285
ronald <ronald-farias@outlook.com>
2019-03-05 18:41:21 -0300

    add obj in example history object and switch to dev get_last_readers

--- src/store/reader/actions.js
+++ src/store/reader/actions.js
@@ -1,4 +1,4 @@
-const dev_history_example = JSON.parse('[{"hostname":"cdmnet.com.br","icon":"http://cdmnet.com.br/favicon.ico","mangas":[{"history":[{"cap":"071","page":"1","url":"http://cdmnet.com.br/titulos/rain/manga/ler-online/071#1"}],"name":"rain"}],"reader":"central-de-mangas"},{"hostname":"onepiece-ex.com.br","icon":"https://onepiece-ex.com.br/favicon/favicon-32x32.png","mangas":[{"history":[{"cap":"931","page":"1","url":"https://onepiece-ex.com.br/mangas/leitor/931/#1"}],"name":"One Piece"}],"reader":"onepiece-ex"}]')
+const dev_history_example = JSON.parse('[{"hostname":"cdmnet.com.br","icon":"http://cdmnet.com.br/favicon.ico","mangas":[{"history":[{"cap":"070","page":"1","url":"http://cdmnet.com.br/titulos/rain/manga/ler-online/071#1"}, {"cap":"071","page":"1","url":"http://cdmnet.com.br/titulos/rain/manga/ler-online/070#1"}],"name":"rain"}],"reader":"central-de-mangas"},{"hostname":"onepiece-ex.com.br","icon":"https://onepiece-ex.com.br/favicon/favicon-32x32.png","mangas":[{"history":[{"cap":"931","page":"1","url":"https://onepiece-ex.com.br/mangas/leitor/931/#1"}],"name":"One Piece"}],"reader":"onepiece-ex"}]')
 
 export function url_domain (fullPath) {
   let a_element = document.createElement('a')
@@ -23,21 +23,21 @@ export function hostname ({commit, dispatch}) {
   })
 }
 
-export function get_last_readers ({commit, dispatch}) {
-  // the original function, uncomment it when build
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.get(['historyReaders'], function(items) {
-      let result = items.historyReaders
-      if (!result) {
-        result = []
-      }
-      resolve(result)
-    })
-  })
-}
-
 // export function get_last_readers ({commit, dispatch}) {
+//   // the original function, uncomment it when build
 //   return new Promise((resolve, reject) => {
-//       resolve(dev_history_example)
+//     chrome.storage.sync.get(['historyReaders'], function(items) {
+//       let result = items.historyReaders
+//       if (!result) {
+//         result = []
+//       }
+//       resolve(result)
 //     })
+//   })
 // }
+
+export function get_last_readers ({commit, dispatch}) {
+  return new Promise((resolve, reject) => {
+      resolve(dev_history_example)
+    })
+}
